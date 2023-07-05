5c087a46213d59604d4f43e3c5c98b50b486960d
Ansar Ismagilov <ansar89kab@bk.ru>
2023-07-05 15:40:57 +0800

    Creating Your First React Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
+import { Component } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      My Todo Application
+      <FirstComponent></FirstComponent>
+      <SecondComponent></SecondComponent>
+      <ThirdComponent></ThirdComponent>
+      <FourthComponent></FourthComponent>
     </div>
   );
+}
+
+function FirstComponent(){
+  return (
+    <div className="FirstComponent">First Component</div>
+  )
+}
+
+function SecondComponent(){
+  return(
+    <div className='SecondComponent'>Second Component</div>
+  )
+}
+
+class ThirdComponent extends Component{
+  render(){
+    return(
+      <div className='ThirdComponent'>Third Component</div>
+    )
+  }
+}
+
+class FourthComponent extends Component{
+  render(){
+    return(
+      <div className='FourthComponent'>Fourth Component</div>
+    )
+  }
 }
 
 export default App;
