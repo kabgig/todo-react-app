2cad7db52d6d1a7d7dbdf1b36dd592bd40987e3d
Ansar Ismagilov <ansar89kab@bk.ru>
2023-07-05 17:42:02 +0800

    Refactoring to Modules 2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import './App.css';
-import FirstComponent from './components/learning-examples/FirstComponent';
-import FourthComponent from './components/learning-examples/FourthComponent';
-import SecondComponent from './components/learning-examples/SecondComponent';
-import ThirdComponent from './components/learning-examples/ThirdComponent';
-import {FifthComponent} from './components/learning-examples/FirstComponent';
+import LearningComponents from './components/learning-examples/LearningComponents';
+
 
 function App() {
   return (
     <div className="App">
-      <FirstComponent/>
-      <SecondComponent/>
-      <ThirdComponent/>
-      <FourthComponent/>
-      <FifthComponent/>
+      <LearningComponents/>
     </div>
   );
 }
-
-
-
 export default App;
---
+++ src/components/learning-examples/LearningComponents.jsx
@@ -0,0 +1,17 @@
+import FirstComponent from './FirstComponent';
+import FourthComponent from './FourthComponent';
+import SecondComponent from './SecondComponent';
+import ThirdComponent from './ThirdComponent';
+import {FifthComponent} from './FirstComponent';
+
+export default function LearningComponents() {
+    return (
+      <div className="LearningComponents">
+        <FirstComponent/>
+        <SecondComponent/>
+        <ThirdComponent/>
+        <FourthComponent/>
+        <FifthComponent/>
+      </div>
+    );
+  }
