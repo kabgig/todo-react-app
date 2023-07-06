320b179f6cb3f72bcc4cbccfc8ef18998f40794a
Ansar Ismagilov <ansar89kab@bk.ru>
2023-07-06 16:37:15 +0800

    additional

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,22 +1,5 @@
-import './Counter.css'
-
-export default function Counter() {
-
-    
-
-    function incrementCounterFunction() {
-        console.log('Increment clicked')
-    }
-
+export default function Counter(){
     return (
-        <div className="Counter">
-            <span className="count">0</span>
-            <div>
-                <button 
-                    className="counterButton" 
-                    onClick={incrementCounterFunction}
-                    >+1</button>
-            </div>
-        </div>
+        <div className="Counter">Counter</div>
     )
 }
