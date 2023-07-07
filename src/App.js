a14f7118af456ae365f45a0019a6291348b1171b
Ansar Ismagilov <ansar89kab@bk.ru>
2023-07-07 15:24:44 +0800

    playing with props

--- src/App.js
+++ src/App.js
@@ -5,8 +5,30 @@ import Counter from './components/counter/Counter.jsx';
 function App() {
   return (
     <div className="App">
+      <PlayingWithProps property1="value1" property2="value2"/>
+      <Counter/>
+      <Counter/>
       <Counter/>
     </div>
   );
 }
+
+// function PlayingWithProps(properties){
+//   console.log(properties)
+//   console.log(properties.property1)
+//   console.log(properties.property2)
+//   return(
+//     <div>Props</div>
+//   )
+// }
+
+function PlayingWithProps({property1, property2}){
+  // console.log(properties)
+  console.log(property1)
+  console.log(property2)
+  return(
+    <div>Props</div>
+  )
+}
+
 export default App;
