7edb2dee7e9ced8bad6d8ac1dc1f76a4d325e1f5
Ansar Ismagilov <ansar89kab@bk.ru>
2023-07-08 10:52:23 +0800

    Getting Started with Login Component - Todo React App

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -2,6 +2,39 @@ export default function TodoApp(){
     return(
         <div className="TodoApp">
             Todo Management Application
+            <LoginComponent/>
+            {/* <WelcomeComponent/> */}
+        </div>
+    )
+}
+
+function LoginComponent(){
+    return(
+        <div className="Login">
+            <div className="LoginForm">
+                <div>
+                    <label>User Name</label>
+                    <input type="text" name="username"/>
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="password" name="password"/>
+                </div>
+                <div>
+                    <button type="button" name="login"/>
+                </div>
+            </div>
+            username
+            password
+            button
+        </div>
+    )
+}
+
+function WelcomeComponent(){
+    return(
+        <div className="Welcome">
+            Welcome Component
         </div>
     )
 }
