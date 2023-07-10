852f98ba57614fc1a86e12da6d5f4a01f8fdc2a4
Ansar Ismagilov <ansar89kab@bk.ru>
2023-07-10 16:02:28 +0800

    Using Bootstrap to Style Todo React Front End

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -5,8 +5,8 @@ import { useState } from 'react'
 export default function TodoApp() {
     return (
         <div className="TodoApp">
-            <HeaderComponent/>
             <BrowserRouter>
+            <HeaderComponent />
                 <Routes>
                     <Route path='/' element={<LoginComponent />} />
                     <Route path='/login' element={<LoginComponent />} />
@@ -15,8 +15,8 @@ export default function TodoApp() {
                     <Route path='/logout' element={<LogoutComponent />} />
                     <Route path='*' element={<ErrorComponent />} />
                 </Routes>
+            <FooterComponent />
             </BrowserRouter>
-            <FooterComponent/>
         </div>
     )
 }
@@ -105,10 +105,10 @@ function ListTodosComponent() {
     const targetDate = new Date(today.getFullYear() + 12, today.getMonth(), today.getDay())
 
     const todos = [
-        { id: 1, description: 'Learn AWS', done: false, targetDate:targetDate },
-        { id: 2, description: 'Learn Java' , done: false, targetDate:targetDate},
-        { id: 3, description: 'Learn Spring', done: false, targetDate:targetDate },
-        { id: 4, description: 'Learn Gradle', done: false, targetDate:targetDate },
+        { id: 1, description: 'Learn AWS', done: false, targetDate: targetDate },
+        { id: 2, description: 'Learn Java', done: false, targetDate: targetDate },
+        { id: 3, description: 'Learn Spring', done: false, targetDate: targetDate },
+        { id: 4, description: 'Learn Gradle', done: false, targetDate: targetDate },
 
     ]
 
@@ -147,21 +147,35 @@ function ListTodosComponent() {
 
 function HeaderComponent() {
     return (
-        <div className="header">
-            <div>
-                Header <hr/>
+        <header className="border-bottom border-light border-5 mb-5 p-2">
+            <div className="container">
+                <div className="row">
+                    <nav className="navbar navbar-expand-lg">
+                        <a className="navbar-brand ms-2 fs-2 fw-bold text-black" href="https://www.in28minutes.com">in28minutes</a>
+                        <div className="collapse navbar-collapse">
+                            <ul className="navbar-nav">
+                                <li className="nav-item fs-5"><Link className="nav-link" to="/welcome/ansar">Home</Link></li>
+                                <li className="nav-item fs-5"><Link className="nav-link" to="/todos">Todos</Link></li>
+                            </ul>
+                        </div>
+                        <ul className="navbar-nav">
+                            <li className="nav-item fs-5"><Link className="nav-link" to="/login">Login</Link></li>
+                            <li className="nav-item fs-5"><Link className="nav-link" to="/logout">Logout</Link></li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
-        </div>
+        </header>
     )
 }
 
 function FooterComponent() {
     return (
-        <div className="footer">
-            <div>
-              <hr/>  footer 
+        <footer className="footer">
+            <div className='container'>
+                Footer
             </div>
-        </div>
+        </footer>
     )
 }
 
