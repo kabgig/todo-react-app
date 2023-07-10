fbaf622878aff097b1f519a679beb74bd75e1ff1
Ansar Ismagilov <ansar89kab@bk.ru>
2023-07-10 12:22:10 +0800

    Conditionally Displaying Messages in Login

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -38,24 +38,10 @@ function LoginComponent() {
         }
     }
 
-    function SuccessMessageComponent() {
-        if (showSuccessMessage) {
-            return <div className="successMessage">Authenticated Successfully</div>
-        }
-        return null
-    }
-
-    function ErrorMessageComponent() {
-        if (showErrorMessage) {
-            return <div className="errorMessage">Authentication Failed. Please check your credentials.</div>
-        }
-        return null
-    }
-
     return (
         <div className="Login">
-            <SuccessMessageComponent/>
-            <ErrorMessageComponent/>
+            {showSuccessMessage && <div className="successMessage">Authenticated Successfully</div>}
+            {showErrorMessage && <div className="errorMessage">Authentication Failed. Please check your credentials.</div>}
              <div className="LoginForm">
                 <div>
                     <label>User Name</label>
