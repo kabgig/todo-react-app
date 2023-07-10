98ba4b5773d3074408eb6bc92b41d710b6785313
Ansar Ismagilov <ansar89kab@bk.ru>
2023-07-10 12:11:36 +0800

    Adding Hardcoded Authentication

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,33 +1,33 @@
 import './TodoApp.css'
 import { useState } from 'react'
 
-export default function TodoApp(){
-    return(
+export default function TodoApp() {
+    return (
         <div className="TodoApp">
-            <LoginComponent/>
+            <LoginComponent />
             {/* <WelcomeComponent/> */}
         </div>
     )
 }
 
-function LoginComponent(){
+function LoginComponent() {
 
-    const [username,setUsername] = useState('in28minutes')
-    const [password,setPassword] = useState('')
-    const [showSuccessMessage,setShowSuccessMessage] = useState(false)
-    const [showErrorMessage,setShowErrorMessage] = useState(false)
-    
-    function handleUserNameChange(event){
+    const [username, setUsername] = useState('ansar')
+    const [password, setPassword] = useState('')
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false)
+    const [showErrorMessage, setShowErrorMessage] = useState(false)
+
+    function handleUserNameChange(event) {
         setUsername(event.target.value);
     }
-    
-    function handlePasswordChange(event){
+
+    function handlePasswordChange(event) {
         console.log(event.target.value);
         setPassword(event.target.value);
     }
 
-    function handleSubmit(){
-        if(username==='ansar' && password==='12345678'){
+    function handleSubmit() {
+        if (username === 'ansar' && password === '12345678') {
             setShowSuccessMessage(true)
             setShowErrorMessage(false)
             console.log('success')
@@ -38,18 +38,32 @@ function LoginComponent(){
         }
     }
 
-    return(
+    function SuccessMessageComponent() {
+        if (showSuccessMessage) {
+            return <div className="successMessage">Authenticated Successfully</div>
+        }
+        return null
+    }
+
+    function ErrorMessageComponent() {
+        if (showErrorMessage) {
+            return <div className="errorMessage">Authentication Failed. Please check your credentials.</div>
+        }
+        return null
+    }
+
+    return (
         <div className="Login">
-            <div className="successMessage">Authenticated Successfully</div>
-            <div className="errorMessage">Authentication Failed. Please check your credentials.</div>
-            <div className="LoginForm">
+            <SuccessMessageComponent/>
+            <ErrorMessageComponent/>
+             <div className="LoginForm">
                 <div>
                     <label>User Name</label>
-                    <input type="text" name="username" value={username} onChange={handleUserNameChange}/>
+                    <input type="text" name="username" value={username} onChange={handleUserNameChange} />
                 </div>
                 <div>
                     <label>Password</label>
-                    <input type="password" name="password" value={password} onChange={handlePasswordChange}/>
+                    <input type="password" name="password" value={password} onChange={handlePasswordChange} />
                 </div>
                 <div>
                     <button type="button" name="login" onClick={handleSubmit}>Login</button>
@@ -59,8 +73,10 @@ function LoginComponent(){
     )
 }
 
-function WelcomeComponent(){
-    return(
+
+
+function WelcomeComponent() {
+    return (
         <div className="Welcome">
             Welcome Component
         </div>
