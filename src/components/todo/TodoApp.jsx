d3c918b133af135fba44c0c98f5f35a0c56937fe
Ansar Ismagilov <ansar89kab@bk.ru>
2023-07-10 15:22:00 +0800

    Creating React Header, Footer and Logout

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -5,15 +5,18 @@ import { useState } from 'react'
 export default function TodoApp() {
     return (
         <div className="TodoApp">
+            <HeaderComponent/>
             <BrowserRouter>
                 <Routes>
                     <Route path='/' element={<LoginComponent />} />
                     <Route path='/login' element={<LoginComponent />} />
                     <Route path='/welcome/:username' element={<WelcomeComponent />} />
                     <Route path='/todos' element={<ListTodosComponent />} />
+                    <Route path='/logout' element={<LogoutComponent />} />
                     <Route path='*' element={<ErrorComponent />} />
                 </Routes>
             </BrowserRouter>
+            <FooterComponent/>
         </div>
     )
 }
@@ -137,6 +140,37 @@ function ListTodosComponent() {
                         }
                     </tbody>
                 </table>
+            </div>
+        </div>
+    )
+}
+
+function HeaderComponent() {
+    return (
+        <div className="header">
+            <div>
+                Header <hr/>
+            </div>
+        </div>
+    )
+}
+
+function FooterComponent() {
+    return (
+        <div className="footer">
+            <div>
+              <hr/>  footer 
+            </div>
+        </div>
+    )
+}
+
+function LogoutComponent() {
+    return (
+        <div className="LogoutComponent">
+            <h1>You're logged out</h1>
+            <div>
+                Thank you for using our App! See yu later!
             </div>
         </div>
     )
