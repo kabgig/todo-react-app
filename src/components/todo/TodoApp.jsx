55c69572db522aa7b73c9b1707562aa1087dae84
Ansar Ismagilov <ansar89kab@bk.ru>
2023-07-12 18:18:25 +0800

    ecure React Routes using Authenticated Route - 2

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import ErrorComponent from './ErrorComponent'
 import HeaderComponent from './HeaderComponent'
 import ListTodosComponent from './ListTodosComponent'
@@ -6,8 +6,15 @@ import LoginComponent from './LoginComponent'
 import LogoutComponent from './LogoutComponent'
 import WelcomeComponent from './WelcomeComponent'
 import AuthProvider from './security/AuthContext'
-
+import { useAuth } from './security/AuthContext'
 import './TodoApp.css'
+
+function AuthenticatedRoute({ children }) {
+    const authContext = useAuth()
+    if (authContext.isAuthenticated)
+        return children
+    return <Navigate to='/' />
+}
 
 export default function TodoApp() {
     return (
@@ -18,9 +25,23 @@ export default function TodoApp() {
                     <Routes>
                         <Route path='/' element={<LoginComponent />} />
                         <Route path='/login' element={<LoginComponent />} />
-                        <Route path='/welcome/:username' element={<WelcomeComponent />} />
-                        <Route path='/todos' element={<ListTodosComponent />} />
-                        <Route path='/logout' element={<LogoutComponent />} />
+
+                        <Route path='/welcome/:username' element={
+                            <AuthenticatedRoute>
+                                <WelcomeComponent />
+                            </AuthenticatedRoute>
+                        } />
+                        <Route path='/todos' element={
+                            <AuthenticatedRoute>
+                                <ListTodosComponent />
+                            </AuthenticatedRoute>
+                        } />
+                        <Route path='/logout' element={
+                            <AuthenticatedRoute>
+                                <LogoutComponent />
+                            </AuthenticatedRoute>
+                        } />
+
                         <Route path='*' element={<ErrorComponent />} />
                     </Routes>
                 </BrowserRouter>
