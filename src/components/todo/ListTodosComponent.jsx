90a3c25aa6b409ea1e4ce91a3dab1a987008e393
Ansar Ismagilov <ansar89kab@bk.ru>
2023-08-01 17:52:38 +0800

    Adding Delete Feature to React Frontend

--- src/components/todo/ListTodosComponent.jsx
+++ src/components/todo/ListTodosComponent.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react"
-import { retrieveAllTodosForUsername } from "./api/TodoApiService";
+import { useEffect, useState } from "react";
+import { deleteTodoApi, retrieveAllTodosForUsernameApi } from "./api/TodoApiService";
 
 export default function ListTodosComponent() {
 
@@ -7,26 +7,41 @@ export default function ListTodosComponent() {
     const targetDate = new Date(today.getFullYear() + 12, today.getMonth(), today.getDay())
 
     const [todos, setTodos] = useState([])
+    const [message, setMessage] = useState(null)
 
     useEffect(() => refreshTodos(), [])
 
     function refreshTodos() {
-        retrieveAllTodosForUsername('in28minutes')
-            .then(responce => {setTodos(responce.data)})
+        retrieveAllTodosForUsernameApi('in28minutes')
+            .then(responce => { setTodos(responce.data) })
+            .catch(error => console.log(error))
+    }
+    function deleteTodo(id) {
+        console.log('clicked' + id)
+        deleteTodoApi('in28minutes', id)
+            .then(
+                () => {
+                    setMessage(`Delete of todo with id = ${id} successful`)
+
+                    refreshTodos()
+                }
+            )
             .catch(error => console.log(error))
     }
 
     return (
         <div className="container">
             <h1>Things You Want To Do</h1>
+            {message && <div className="alert alert-warning">{message}</div>}
+        
             <div>
                 <table className='table'>
                     <thead>
                         <tr>
-                            <td>ID</td>
-                            <td>Description</td>
-                            <td>Is Done?</td>
-                            <td>Target Date</td>
+                            <th>Description</th>
+                            <th>Is Done?</th>
+                            <th>Target Date</th>
+                            <th>Delete</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -39,6 +54,8 @@ export default function ListTodosComponent() {
                                         <td>{todo.done.toString()}</td>
                                         {/* <td>{todo.targetDate.toDateString()}</td> */}
                                         <td>{todo.targetDate.toString()}</td>
+                                        <td><button className="btn btn-warning"
+                                            onClick={() => deleteTodo(todo.id)}>Delete</button></td>
                                     </tr>
                                 )
                             )
--- src/components/todo/api/TodoApiService.js
+++ src/components/todo/api/TodoApiService.js
@@ -2,9 +2,12 @@ import axios from 'axios'
 
 const apiClient = axios.create(
     {
-        baseURL:'http://localhost:8080'
+        baseURL: 'http://localhost:8080'
     }
 );
 
-export const retrieveAllTodosForUsername
+export const retrieveAllTodosForUsernameApi
     = (username) => apiClient.get(`users/${username}/todos`)
+
+export const deleteTodoApi
+    = (username, id) => apiClient.delete(`users/${username}/todos/${id}`)
