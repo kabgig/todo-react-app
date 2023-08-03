05bb82e7c1ac0462157223c22b82407170099d27
Ansar Ismagilov <ansar89kab@bk.ru>
2023-08-03 16:44:44 +0800

    Adding Update Feature to React Frontend

--- src/components/todo/TodoComponent.jsx
+++ src/components/todo/TodoComponent.jsx
@@ -1,5 +1,5 @@
-import { useParams } from "react-router-dom"
-import { retrieveTodoApi } from "./api/TodoApiService"
+import { useNavigate, useParams } from "react-router-dom"
+import { retrieveTodoApi, updateTodoApi } from "./api/TodoApiService"
 import { useAuth } from "./security/AuthContext"
 import { useEffect, useState } from "react"
 import { ErrorMessage, Field, Form, Formik } from "formik"
@@ -8,6 +8,7 @@ import { ErrorMessage, Field, Form, Formik } from "formik"
 export default function TodoComponent() {
     const { id } = useParams()
     const authContext = useAuth()
+    const navigate = useNavigate()
     const username = authContext.username
     const [description, setDescription] = useState('')
     const [targetDate, setTargetDate] = useState('')
@@ -25,6 +26,17 @@ export default function TodoComponent() {
 
     function onSubmit(values) {
         console.log(values)
+        const todo = {
+            id: id,
+            username: username,
+            description: values.description,
+            targetDate: values.targetDate,
+            done: false
+        }
+        console.log(todo)
+        updateTodoApi(username, id, todo)
+            .then(responce => {navigate('/todos')})
+            .catch(error => console.log(error))
     }
 
     function validate(values) {
@@ -52,8 +64,8 @@ export default function TodoComponent() {
                     enableReinitialize={true}
                     onSubmit={onSubmit}
                     validate={validate}
-                    validateOnChange = {false}
-                    validateOnBlur = {false}>
+                    validateOnChange={false}
+                    validateOnBlur={false}>
                     {
                         (props) => (
                             <Form>
