d36dcdc8391a63f8d3d74e5d7a7085976d6587f2
Ansar Ismagilov <ansar89kab@bk.ru>
2023-08-04 15:38:27 +0800

    Calling Basic Authentication Service when Logging

--- src/components/todo/api/HelloWorldApiService.js
+++ src/components/todo/api/HelloWorldApiService.js
@@ -6,11 +6,22 @@ import axios from 'axios'
 
 const apiClient = axios.create(
     {
-        baseURL:'http://localhost:8080'
+        baseURL: 'http://localhost:8080'
     }
 );
 
-export const retrieveHelloWorldBean 
+export const retrieveHelloWorldBean
     = () => apiClient.get('/hello-world-bean')
-export const retrieveHelloWorldPathVariable 
-    = (username) => apiClient.get(`/hello-world/path-variable/${username}`)
+export const retrieveHelloWorldPathVariable
+    = (username) => apiClient.get(`/hello-world/path-variable/${username}`, {
+        headers: {
+            Authorization: 'Basic aW4yOG1pbnV0ZXM6MTIzNDU2Nzg='
+        }  
+    })
+
+export const executeBasicAuthenticationService
+    = (token) => apiClient.get(`/basicauth`, {
+        headers: {
+            Authorization: token
+        }
+    })
--- src/components/todo/security/AuthContext.js
+++ src/components/todo/security/AuthContext.js
@@ -1,4 +1,6 @@
 import { createContext, useState, useContext } from "react";
+import { executeBasicAuthenticationService } from "../api/HelloWorldApiService";
+
 
 
 export const AuthContext = createContext()
@@ -10,16 +12,37 @@ export default function AuthProvider({ children }) {
     const [username, setUsername] = useState(null)
     const valueToBeShared = { isAuthenticated, login, logout, username}
 
+    // function login(username,password){
+    //     if (username === 'in28minutes' && password === '12345678') {
+    //         setAuthenticated(true)
+    //         setUsername(username)
+    //         return true
+    //     } else {
+    //         setAuthenticated(false)
+    //         setUsername(null)
+    //         return false
+    //     }
+    // }
+
     function login(username,password){
-        if (username === 'in28minutes' && password === '12345678') {
-            setAuthenticated(true)
-            setUsername(username)
-            return true
-        } else {
-            setAuthenticated(false)
-            setUsername(null)
-            return false
-        }
+
+        const baToken = 'Basic ' + window.btoa(username + ":" + password)
+
+        executeBasicAuthenticationService(baToken)
+        .then(response => console.log(response))
+        .catch(error => console.log(error))
+
+        setAuthenticated(false)
+
+        // if (username === 'in28minutes' && password === '12345678') {
+        //     setAuthenticated(true)
+        //     setUsername(username)
+        //     return true
+        // } else {
+        //     setAuthenticated(false)
+        //     setUsername(null)
+        //     return false
+        // }
     }
 
     function logout(){
